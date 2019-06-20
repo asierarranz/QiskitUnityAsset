313addce95b15092400842c3b2603463a583c407
Korgan00 <korgan00@gmail.com>
2019-06-20 20:41:53 +0200

    fix

--- FlaskServer/api.py
+++ FlaskServer/api.py
@@ -6,7 +6,7 @@ from qiskit import QuantumCircuit, Aer, execute, IBMQ
 
 def run_qasm(qasm, backend_to_run="qasm_simulator", api_token=None, shots=1024, memory=False):
     if api_token:
-        IBMQ.enable_account(api_token)
+        IBMQ.enable_account(api_token, 'https://api.quantum-computing.ibm.com/api/Hubs/ibm-q/Groups/open/Projects/main')
     qc = QuantumCircuit.from_qasm_str(qasm)
     backend = Aer.get_backend(backend_to_run)
     job_sim = execute(qc, backend, shots=shots, memory=memory)
